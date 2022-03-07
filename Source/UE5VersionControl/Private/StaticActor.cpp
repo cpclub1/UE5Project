cb54079e8b3f2d8971225bc1f460b81cbda754b3
Jingyu Kim <cpclub1@gmail.com>
2022-03-07 12:33:03 +0900

    Changes  StaticActor and map

--- Source/UE5VersionControl/Private/StaticActor.cpp
+++ Source/UE5VersionControl/Private/StaticActor.cpp
@@ -9,6 +9,7 @@ AStaticActor::AStaticActor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// testing for git
 }
 
 // Called when the game starts or when spawned
